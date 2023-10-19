ea106e29f8e38b332e15ffb76f0df91c84f9ee52
Stephen Buonamassa <sjbuonamassa@gmail.com>
2023-10-19 14:37:11 -0400

    Added pokemon caching to fusion route

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 To get running, make sure you have rust installed. Then run `cargo run` and you're set!
 
+# Features
+
+- Pokemon caching for requested pokemon
+
 # Future Plans
 
 This is just a fun project to help myself learn rust! Not sure what will come of it but here are some future plans:
 
-- Cache pokemon from API
 - Calculate fusion stats
 - Cache fusion pokemon as requested
 - Host images of fused pokemon (and possibly unfused)
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
 use axum::{
+    debug_handler,
     routing::get,
-    Router, extract::Path,
+    Router, extract::{ Path, State},
 };
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
+
 use serde_json::Value;
+
+
+type SharedState = Arc<RwLock<AppState>>;
+
+#[derive(Clone, Default)]
+struct AppState {
+    unfused_pokemon: HashMap<u16, Pokemon>,
+}
 
 
 #[tokio::main]
 async fn main() {
-    let mut collected_pokemon: HashMap<u16, Pokemon> = HashMap::new();
+    let shared_state = SharedState::default();
 
     // build our application with a single route
     let app = Router::new()
         .route("/", get(ping))
         .route("/pokemon/name/:id", get(name_handler))
-        .route("/pokemon/fuse/:id1/:id2", get(fuse_handler));
+        .route("/pokemon/fuse/:id1/:id2", get(fuse_handler))
+        .with_state(Arc::clone(&shared_state));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -28,6 +42,7 @@ async fn ping() -> &'static str {
     "Welcome to the pokemon fusion API!"
 }
 
+#[derive(Clone)]
 struct Stats {
     hp: u16,
     attack: u16,
@@ -37,35 +52,78 @@ struct Stats {
     speed: u16,
 }
 
+#[derive(Clone)]
 struct Pokemon {
     name: String,
     stats: Stats,
     types: (String, String)
 }
 
-async fn fuse_handler(Path((id1, id2)): Path<(u16, u16)>) -> String {
+#[debug_handler]
+async fn fuse_handler(Path((id1, id2)): Path<(u16, u16)>, State(state): State<SharedState>) -> String {
 
-    let p1 : Pokemon;
-    let p2 : Pokemon;
+    let mut p1 : Option<Pokemon> = None;
+    let mut p2 : Option<Pokemon> = None;
 
-    let pokemon_1_r = get_pokemon_data(id1).await;
-    match pokemon_1_r {
-        Err(_err) => return "Failed to grab pokemon 1".to_string(),
-        Ok(pkmn) => {p1 = pkmn},
+    {
+        let db = &state.read().unwrap().unfused_pokemon;
+
+        if let Some(pkmn) = db.get(&id1) {
+            p1 = Some(pkmn.clone());
+            println!("Using cache for pokemon 1.");
+        }
     }
 
-    let pokemon_2_r = get_pokemon_data(id2).await;
-    match pokemon_2_r {
-        Err(_err) => return "Failed to grab pokemon 2".to_string(),
-        Ok(pkmn) => {p2 = pkmn},
+    if p1.is_none() {
+        println!("Using API for pokemon 1.");
+        let pokemon_1_r = get_pokemon_data(id1).await;
+        match pokemon_1_r {
+            Err(_err) => return "Failed to grab pokemon 1".to_string(),
+            Ok(pkmn) => {
+                state.write().unwrap().unfused_pokemon.insert(id1, pkmn.clone());
+                p1 = Some(pkmn);
+            },
+        }
     }
 
-    format!("Fusing {} with {}", p1.name, p2.name )
+    {
+        let db = &state.read().unwrap().unfused_pokemon;
+
+        if let Some(pkmn) = db.get(&id2) {
+            p2 = Some(pkmn.clone());
+            println!("Using cache for pokemon 2.");
+        }
+    }
+
+    if p2.is_none() {
+        println!("Using API for pokemon 2.");
+        let pokemon_2_r = get_pokemon_data(id2).await;
+        match pokemon_2_r {
+            Err(_err) => return "Failed to grab pokemon 2".to_string(),
+            Ok(pkmn) => {
+                state.write().unwrap().unfused_pokemon.insert(id2, pkmn.clone());
+                p2 = Some(pkmn);
+            },
+        }
+    } 
+
+    match p1 {
+        Some(pkmn1) => {
+            match p2 {
+                Some(pkmn2) => {
+                    format!("Fusing {} with {}", pkmn1.name, pkmn2.name)
+                },
+                None => {"Failed to get pokemon 2 data".to_string()}
+            }
+        },
+        None => {"Failed to get pokemon 1 data".to_string()}
+    }
 }
 
-async fn name_handler(Path(id): Path<u16>) -> String {
+async fn name_handler(Path(id): Path<u16>, State(state): State<SharedState>) -> String {
     let resp = get_pokemon_data(id).await;
 
+    
     match resp {
         Err(err) => "Failed to grab".to_string(),
         Ok(msg) => msg.name
@@ -73,7 +131,6 @@ async fn name_handler(Path(id): Path<u16>) -> String {
 }
 
 async fn get_pokemon_data(id: u16) -> Result<Pokemon,reqwest::Error>  {
-    
     let url = format!("https://pokeapi.co/api/v2/pokemon/{}", id);
     let resq: serde_json::Value = reqwest::get(url).await?
     .json()
