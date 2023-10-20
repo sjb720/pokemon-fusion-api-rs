10fcca1c3a73471b4710249b11f02a162df75cd2
Stephen Buonamassa <sjbuonamassa@gmail.com>
2023-10-19 21:43:59 -0400

    Added fusion stats, name, and types for pokemon. Fusion route now returns a JSON instead of just the names of the two pokemon to be fused.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use axum::{
     debug_handler,
     routing::get,
     Router, extract::{ Path, State},
+    Json
 };
 use std::{
     collections::HashMap,
@@ -9,6 +10,7 @@ use std::{
 };
 
 use serde_json::Value;
+use serde::Serialize;
 
 
 type SharedState = Arc<RwLock<AppState>>;
@@ -41,7 +43,7 @@ async fn ping() -> &'static str {
     "Welcome to the pokemon fusion API!"
 }
 
-#[derive(Clone)]
+#[derive(Serialize, Clone)]
 struct Stats {
     hp: u16,
     attack: u16,
@@ -51,7 +53,7 @@ struct Stats {
     speed: u16,
 }
 
-#[derive(Clone)]
+#[derive(Serialize, Clone)]
 struct Pokemon {
     name: String,
     stats: Stats,
@@ -59,7 +61,7 @@ struct Pokemon {
 }
 
 #[debug_handler]
-async fn fuse_handler(Path((id1, id2)): Path<(u16, u16)>, State(state): State<SharedState>) -> String {
+async fn fuse_handler(Path((id1, id2)): Path<(u16, u16)>, State(state): State<SharedState>) -> Json<Pokemon> {
 
     let mut p1 : Option<Pokemon> = None;
     let mut p2 : Option<Pokemon> = None;
@@ -77,7 +79,7 @@ async fn fuse_handler(Path((id1, id2)): Path<(u16, u16)>, State(state): State<Sh
         println!("Using API for pokemon 1.");
         let pokemon_1_r = get_pokemon_data(id1).await;
         match pokemon_1_r {
-            Err(_err) => return "Failed to grab pokemon 1".to_string(),
+            Err(_err) => {},
             Ok(pkmn) => {
                 state.write().unwrap().unfused_pokemon.insert(id1, pkmn.clone());
                 p1 = Some(pkmn);
@@ -98,7 +100,7 @@ async fn fuse_handler(Path((id1, id2)): Path<(u16, u16)>, State(state): State<Sh
         println!("Using API for pokemon 2.");
         let pokemon_2_r = get_pokemon_data(id2).await;
         match pokemon_2_r {
-            Err(_err) => return "Failed to grab pokemon 2".to_string(),
+            Err(_err) => {},
             Ok(pkmn) => {
                 state.write().unwrap().unfused_pokemon.insert(id2, pkmn.clone());
                 p2 = Some(pkmn);
@@ -110,12 +112,21 @@ async fn fuse_handler(Path((id1, id2)): Path<(u16, u16)>, State(state): State<Sh
         Some(pkmn1) => {
             match p2 {
                 Some(pkmn2) => {
-                    format!("Fusing {} with {}", pkmn1.name, pkmn2.name)
+                    let fused = fuse_pokemon(&pkmn1, &pkmn2);
+                    return Json(fused);
                 },
-                None => {"Failed to get pokemon 2 data".to_string()}
+                None => {
+                    Json(
+                        Pokemon { name: "MISSING NO".to_string(), stats: Stats { hp: 1, attack: 1, defense: 1, special_attack: 1, special_defense: 1, speed: 1 }, types: ("none".to_string(), "none".to_string()) }
+                    )
+                }
             }
         },
-        None => {"Failed to get pokemon 1 data".to_string()}
+        None => {
+            Json(
+                Pokemon { name: "MISSING NO".to_string(), stats: Stats { hp: 1, attack: 1, defense: 1, special_attack: 1, special_defense: 1, speed: 1 }, types: ("none".to_string(), "none".to_string()) }
+            )
+        }
     }
 }
 
@@ -129,12 +140,50 @@ async fn get_pokemon_data(id: u16) -> Result<Pokemon,reqwest::Error>  {
 }
 
 fn pokemon_api_to_struct(data : &Value) -> Pokemon {
+
+    let types: &Vec<Value> = data["types"].as_array().unwrap();
+    let type_touple: (String, String);
+
+    if types.len() == 1 {
+        type_touple = (data["types"][0]["type"]["name"].as_str().unwrap_or_default().to_string(), "none".to_string())
+    } else {
+        type_touple = (
+            data["types"][0]["type"]["name"].as_str().unwrap_or_default().to_string(), 
+            data["types"][1]["type"]["name"].as_str().unwrap_or_default().to_string()
+        )
+    }
+
     Pokemon { name: data["name"].as_str().unwrap_or_default().to_string(), stats: Stats {
-        attack: 0,
-        defense: 0,
-        hp: 0,
-        special_attack: 0,
-        special_defense: 0,
-        speed: 0,
-    }, types: ("psychic".to_string(), "grass".to_string()) }
+        hp: data["stats"][0]["base_stat"].as_u64().unwrap_or_default() as u16,
+        attack: data["stats"][1]["base_stat"].as_u64().unwrap_or_default() as u16,
+        defense: data["stats"][2]["base_stat"].as_u64().unwrap_or_default() as u16,
+        special_attack: data["stats"][3]["base_stat"].as_u64().unwrap_or_default() as u16,
+        special_defense: data["stats"][4]["base_stat"].as_u64().unwrap_or_default() as u16,
+        speed: data["stats"][5]["base_stat"].as_u64().unwrap_or_default() as u16,
+    }, types: type_touple }
+}
+
+fn fuse_pokemon(pokemon1: &Pokemon, pokemon2: &Pokemon) -> Pokemon {
+
+    let second_fuse_type : String;
+    match pokemon2.types.1.as_str() {
+        "none" => {second_fuse_type = pokemon2.types.0.clone()},
+        _ => {second_fuse_type= pokemon2.types.1.clone()}
+    }
+
+    Pokemon {
+        name: format!("{}{}",&pokemon1.name[0..(pokemon1.name.len()/2)], &pokemon2.name[(pokemon2.name.len()/2)..pokemon2.name.len()] ),
+        stats: Stats {
+            attack: (pokemon1.stats.attack * 2 + pokemon2.stats.attack) / 3,
+            defense: (pokemon1.stats.defense * 2 + pokemon2.stats.defense) / 3,
+            hp: (pokemon1.stats.hp * 2 + pokemon2.stats.hp) / 3,
+            special_attack: (pokemon1.stats.special_attack * 2 + pokemon2.stats.special_attack) / 3,
+            special_defense: (pokemon1.stats.special_defense * 2 + pokemon2.stats.special_defense) / 3,
+            speed: (pokemon1.stats.attack * 2 + pokemon2.stats.speed) / 3,
+        },
+        types: (
+            pokemon1.types.0.clone(),
+            second_fuse_type
+        )
+    }
 }
